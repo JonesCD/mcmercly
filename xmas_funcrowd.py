c119360573f125069655d08ac3729246533c7342
Christopher Jones <jones.christopherd@gmail.com>
2017-12-02 23:34:21 -0500

    Update xmas_funcrowd.py
    updated for 2017 to add Reindeer and minor rules changes (2 person loops now allowed)

--- xmas_funcrowd.py
+++ xmas_funcrowd.py
@@ -1,7 +1,6 @@
 import random
-from datetime import datetime
 
-peeps = ['erika', 'alex', 'brian', 'olessa', 'chris', 'sarah', 'charlie']
+peeps = ['alex', 'brian', 'olessa', 'chris', 'sarah', 'charlie', 'erika']
 peeps_dict = {}
 picks_dict = {}
 santa = {}
@@ -16,7 +15,6 @@ def matching(person):
 	global picks_dict
 
 	hat_pull = random.random()
-	print(hat_pull)
 
 	diff = float('inf')
 	for key, value in peeps_dict.items():
@@ -33,12 +31,31 @@ def matching(person):
 	# can't already be selected
 	elif match in picks_dict.values():
 		matching(person)
-	# can't have same person that has you
-	elif match in picks_dict.keys(): 
-		if picks_dict[match] == person:
-			matching(person)
-		else:
-			picks_dict[person] = match
+	else:
+		picks_dict[person] = match
+
+	return(picks_dict)
+
+def matching_reindeer(person):
+	global peeps_dict
+	global picks_dict
+
+	hat_pull = random.random()
+
+	diff = float('inf')
+	for key, value in peeps_dict.items():
+		if diff > abs(hat_pull - value):
+			diff = abs(hat_pull - value)
+			match = key
+
+	# can't be self
+	if match == person:
+		matching(person)
+	# CAN be significant other
+
+	# can't already be selected
+	elif match in picks_dict.values():
+		matching(person)
 	else:
 		picks_dict[person] = match
 
@@ -67,12 +84,33 @@ def go_elf():
 	elf = picks_dict
 	return(elf)
 
-go_santa()
-go_elf()
-#print(santa)
-#print(elf)
+def go_reindeer():
+	global picks_dict
+	global santa
+	global elf
+	global reindeer
+	picks_dict = {}
+	for i in peeps:
+		matching_reindeer(i)
+	#can't have same reindeer as santa or elf
+	for person in picks_dict.keys():
+		if picks_dict[person] == santa[person]:
+			go_reindeer()
+		elif picks_dict[person] == elf[person]:
+			go_reindeer()
+	reindeer = picks_dict
+	return(reindeer)
 
-# email the results by person
+
+
+go_santa()
+print(santa)
+go_elf()
+print(elf)
+go_reindeer()
+print(reindeer)
+
+#email the results by person
 def send_email(user, pwd, recipient, subject, body):
     import smtplib
 
@@ -90,17 +128,17 @@ def send_email(user, pwd, recipient, subject, body):
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.ehlo()
         server.starttls()
-        server.login(gmail_user, gmail_pwd)
+        server.login('jones.christopherd@gmail.com', '')
         server.sendmail(FROM, TO, message)
         server.close()
         print('successfully sent the mail')
     except:
         print("failed to send mail")
 
-peeps_addy_dict = {'chris': 'jones.christopherd@gmail.com', 'olessa', 'alex', 'charlie', 'erika', 'sarah', 'brian'}
+peeps_addy_dict = {'chris': 'jones.christopherd@gmail.com', 'olessa': '', 'alex': '', 'charlie': '', 'erika': '', 'sarah': '', 'brian': ''}
 
+msg = "Hello, member of the Fun Crowd (%s)! We are, again having a group gift exchange. A Secret Santa. A now habitual celebration of the solstice/season/xmas/holidays/winter/&c. via randomly assigned gifting and surprises! The THIRD annual event!\n\nLast year we appeared to be successful at avoiding intra-household collusion, so let's keep that up! That word, 'collusion', is much more powerful now. Please note that this is an email message to you, and you only. For your eyes only, in spy-speak (though, it is email, so maybe for Putin's also (too soon?) (joke left in from 2016)). Yes, it did come from my personal account, but you can be assured, I have the discipline to never bother looking in my sent-items folder. Maybe it isn't discipline, but the lack of a need to live in the past. Feel free to discuss that point with anyone you choose, but DO NOT DISCUSS YOUR GIFTING ASSIGNMENTS WITH ANYONE!\n\nTo remind you of the structure: you're 'Santa' to someone. That means you will be very thoughtful (again, all by yourself - you're a big kid, and I know you can handle this), and buy/craft/bake/commission/manufacture/find them a gift with a value (including tax and shipping (do NOT prorate your Amazon Prime membership)) of less than $20. This is strictly, though not violently, enforced.\n\nYou are also 'Elf' to someone. Same rules, except the spending limit is $5.\n\nYou are now, THIS YEAR!, also a 'Reindeer' to someone. You are NOT to spend a single cent on this person. This limitation is highly restrictive - if you make them something you CANNOT buy new supplies, unless they happen to be supplies for another project ALREADY planned, or use current supplies if you have another project planned for them. If you give them something you already own, you CANNOT have been contemplating a Craigslist ad for said item, or have another plan for deriving value from it. You shall NOT give trash or other detritus from the street. Be clever.\n\nRemember to label your gifts with a 'To: Person, From: Santa|Elf|Reindeer'.\n\n\n\nYou are Santa to %s, Elf to %s, and Reindeer to %s.\n\n\n\nIf the parenthetical name in the first sentence is you, or if you've been assigned to Santa, Elf, or Reindeer to yourself, hit me with a text message - something has gone very very wrong.\n\nBe Merry,\nChris \n\n\n\nP.S. Rules have modified this year for the sake of simplicity. You will not Santa or Elf within your own household, but Reindeer is possible. All three should be unique people. I used to limit it so that if you were Santa to someone they couldn't Santa to you, but that limitation proved to limiting with our small group, and I don't think we need to invite 5 or 10 more people to make that work better. I like you guys and tend to dislike stangers. Have an eggnog."
 
-msg = "Hello, member of the Fun Crowd (%s)! We are, again having a group gift exchange. A Secret Santa. A now habitual celebration of the solstice/season/xmas/holidays/winter/etc via randomly assigned gifting and surprises! The second annual event!\n\nLast year we suffered a major failing of the rules, in that there was horrendous collusion behind closed doors. Please note that this is an email message to you, and you only. For your eyes only, in spy-speak (though, it is email, so maybe for Putin's also (too soon?)). Yes, it did come from my personal account, but you can be assured, I have the discipline to never bother looking in my sent-items folder. Maybe it isn't discipline, but the lack of a need to live in the past. Feel free to discuss that point with anyone you choose, but DO NOT DISCUSS YOUR GIFTING ASSIGNMENTS WITH ANYONE!\n\nTo remind you of the structure: you're 'Santa' to someone. That means you will be very thoughtful (again, all by yourself - you're a big kid, and I know you handle this), and buy/craft/bake/commission/manufacture/find them a gift with a value (including tax and shipping (do NOT prorate your Amazon Prime membership)) of less than $15. This is strictly enforced.\n\nYou are also 'Elf' to someone. Same rules, except the spending limit is $3. You can see that all together this is less than a Hamilton, and really, since the Broadway sensation isn't everything lesser than Hamilton?\n\nRemember to label your gifts with a 'To: Person, From: Santa|Elf'.\n\n\n\nYou are Santa to %s and Elf to %s.\n\n\n\nIf the parenthetical name in the first sentence is you, or if you've been assigned to Santa or Elf yourself, hit me with a text message - something has gone very very wrong.\n\nBe Merry,\nChris"
 
 for i in peeps:
-	send_email('jones.christopherd@gmail.com', '', peeps_addy_dict[i], 'Gift Exchange - 12/10/16 - Up the Hudson Edition!', msg % (i, santa[i], elf[i]))
+	send_email('jones.christopherd@gmail.com', '', peeps_addy_dict[i], 'Gift Exchange 2017 - Third time means three secrets!', msg % (i, santa[i], elf[i], reindeer[i]))
